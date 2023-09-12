ed186e8aeab2727023b6c755084a59c16e470352
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-12 10:59:59 -0700

    lo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,23 +10,29 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		_putchar(48);
-		for (b = 1; b <= 9; b++)
+		
+		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			_putchar(44);
-			_putchar(32);
-			if (c <= 9)
+			if (b == 0)
 			{
-				_putchar(32);
-				_putchar(c + 48);
+				_putchar(c +'0');
 			}
-			else
+			if (c < 10 && b != 0)
 			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar(' ')
+                                _putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 	_putchar('/n');
