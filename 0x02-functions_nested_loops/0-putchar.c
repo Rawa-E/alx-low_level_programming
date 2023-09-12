f48953e5f766af06ba51d252ed47836dcaec9b34
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-12 06:46:53 -0700

    yt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: print a qoute using write function
- *      ssize_t write(int fd.const void buf.size_t count)
+ * Description: prints _putchar using putchar prototype
  *
- * Return: 1 (not success)
+ * Return: 0 (success)
 */
 
 int main(void)
