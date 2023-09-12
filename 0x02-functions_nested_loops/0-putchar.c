c6d417394411aebd595d0fa4b818bbb5ad53676c
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-12 06:33:12 -0700

    one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *      ssize_t write(int fd.const void buf.size_t count)
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	-put('/n');
+
+	return (0);
