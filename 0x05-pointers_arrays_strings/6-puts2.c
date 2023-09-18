817aff610b85697030948293723fd88d2edc9cf4
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-18 10:16:05 -0700

    o

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 
 /**
- * puts2 - prints every other character of a string
- *
- * @str: Input string
- *
- * Return: void
- *
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (str[i] != '\0')
+	while (count >= 0)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-
-	_putchar('\n');
 }
