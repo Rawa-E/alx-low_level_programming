a6665e7a78818e8bdc4794523b784bca59a18c85
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-21 18:37:06 -0700

    ol

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
+
 void print_number(int n)
 {
-	/* if n is divisible by 10 */
-	if (n / 10)
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
 	{
-		/* call function again and evaluate */
-		print_number(n / 10);
-		/* take second digit and store in n */
-		n %= 10;
-		/* if second digit negative */
-		if (n < 0)
-			/* make it positive */
-			n *= -1;
+		_putchar('-');
+		n1 = -n;
 	}
-	/* if n is NOT divisible by 10 */
-	else
-		/* and if n is negative */
-		if (n < 0)
-		{
-			/* print a minus sign */
-			putchar('-');
-			/* and make n positive */
-			n *= -1;
-		}
-	/* print out n */
-	putchar(n + '0');
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
