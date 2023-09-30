62b35280c91a910d1e217c55871ca9aaa2dbd9a3
Rawa-E <rawaelgiaily2001@gmail.com>
2023-09-29 18:19:31 -0700

    p

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
+
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
