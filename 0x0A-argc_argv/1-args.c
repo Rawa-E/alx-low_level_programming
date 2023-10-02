2239fc21fda4c807875122ff64597c39cad8c880
Rawa-E <rawaelgiaily2001@gmail.com>
2023-10-02 14:27:15 -0700

    lk

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,45 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 
-void print_array_elements(char *str);
-
 /**
- * main - Prints all arguments it receives
- *
- * @argc: Length of @argv, integer
- *
- * @argv: Array of strings of the arguments of this program
- *
- * Return: 0, success
+ * main - prints the number of arguments passed into it.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		print_array_elements(argv[i]);
-		_putchar('\n');
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
-}
-
-/**
- * print_array_elements - Prints all char of a string
- *
- * @str: Pointer to string
- *
- * Return: void
- */
-
-void print_array_elements(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
 }
