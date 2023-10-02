e766a01b4fd2794e084665160d2343391575ebf2
Rawa-E <rawaelgiaily2001@gmail.com>
2023-10-02 14:22:12 -0700

    h

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers followed by a new line
+ * @argc: count
+ * @argv: value
+ * Return: 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	int j, mul = 1;
+
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (j = 1; j <= 2; j++)
+			mul = mul * atoi(argv[j]);
+		printf("%d\n", mul);
+		return (0);
+	}
+}
