94df8e78fefd46c54d1b55093d1a79bb165cbf47
Rawa-E <rawaelgiaily2001@gmail.com>
2023-10-12 06:23:52 -0700

    l

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,30 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
-
+#include <string.h>
 /**
  * main - Entry point
- *
- * @argc: length of command line arguments
- *
- * @argv: double pointer to cli arguments
- *
- * Return: 0
+ * @argc: integer
+ * @argv: value
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int (*func_ptr)(int, int);
+	int a, b, (*op_func)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	func_ptr = get_op_func(argv[2]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
 
-	if (func_ptr == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
-
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
